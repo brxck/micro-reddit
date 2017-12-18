bccde2abb2ea8030a5cf0793e5c09b5533c2a5fa
Brock McElroy <28877984+brxck@users.noreply.github.com>
2017-12-18 16:15:44 -0700

    Add user

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates :name, presence: true,
+                   uniqueness: { case_sensitive: false },
+                   length: { minimum: 3, maximum: 16 },
+                   format: { with: /\w/ }
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+end
